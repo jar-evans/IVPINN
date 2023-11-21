edf6ee04d03e976908ddeb2928e5896389d9a174
Mariano <mariano.biondo@mail.polimi.it>
2023-11-21 11:26:12 +0000

    working/VPINN folder working

--- working/VPINN/VPINN_tri_final.py
+++ working/VPINN/VPINN_tri_final.py
@@ -12,7 +12,7 @@ from quad import *
 
 
 
-class VPINN(tf.keras.Model):
+class VPINN():
 
     def __init__(self, pb, params, mesh, NN = None):
 
@@ -20,20 +20,23 @@ class VPINN(tf.keras.Model):
 
         # accept parameters
         self.pb = pb
+
         self.params = params
         self.n_test = params['n_test']
 
         self.mesh = mesh
+
         self.n_vertices=len(mesh['vertices'])
         self.n_edges=len(mesh['edges'])
         self.n_triangles=len(mesh['triangles'])
         self.dof=(self.n_vertices-np.sum(self.mesh['vertex_markers']))+(self.n_edges-np.sum(self.mesh['edge_markers']))
-        print(self.dof)
 
-        self.n_el_x, self.n_el_y = self.params['n_elements']
+
 
         # generate all points/coordinates to be used in the process
         self.generate_boundary_points()
+
+
         # self.generate_inner_points()
         self.pre_compute()
 
@@ -42,8 +45,8 @@ class VPINN(tf.keras.Model):
             self.set_NN(NN)
 
     def set_NN(self, NN, LR=0.01):
-        np.random.seed(SEED)
-        tf.random.set_seed(SEED)
+ 
+
         # initialise the NN
         self.NN = NN
 
@@ -52,6 +55,7 @@ class VPINN(tf.keras.Model):
 
         # set optimiser
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=LR)
+
 
 
     @tf.function
@@ -93,13 +97,14 @@ class VPINN(tf.keras.Model):
 
     
     def u_NN(self, x, y):
-        eval=tf.constant([[x,y]],dtype=tf.float64)
+        eval=tf.constant([[x,y]],dtype=tf_type)
         return self.NN(eval).numpy()[0,0] 
 
 
     @tf.function
     def boundary_loss(self):
     ## NOTE:impose boundary or same structure for ICs
+
         prediction = self.eval_NN(self.boundary_points)
         u_bound_exact=self.u_bound_exact
         return tf.reduce_mean(tf.square(u_bound_exact - prediction))
@@ -225,134 +230,6 @@ class VPINN(tf.keras.Model):
         return (tf.reduce_sum(tf.square(sum_of_vectors_vertices-F_total_vertices))+tf.reduce_sum(tf.square(sum_of_vectors_edges-F_total_edges)))/self.dof
 
 
-    #@tf.function
-    def variational_loss(self):
-
-
-
-        a_vertices = self.a_vertices #
-        a_edges = self.a_edges #
-
-        n_triangles=self.n_triangles
-        xy_quad_total =self.xy_quad_total
-        dof=self.dof
-
-        w_quad = tf.concat([self.w_quad.T,self.w_quad.T], axis=0)
-
-
-        #eval in one shot 
-        x_eval=tf.reshape(xy_quad_total,(-1,2))
-
-        grad=self.eval_grad_NN(x_eval)
-        grad_=tf.reshape(grad,(n_triangles,-1,2))
-
-        F_total_vertices=self.F_total_vertices
-        F_total_edges=self.F_total_edges
-
-   
-
-        grad_test=self.grad_test  
-
-
-
-
-        res=0.0
-
-        for index,triangle in enumerate(self.mesh['triangles']):
-
-
-
-            grad_elem=tf.transpose(grad_[index])
-   
-
-            # get quadrature points in arb. element and get jacobian
-            B,c,J,B_D,B_DD=self.b.change_of_coordinates(self.mesh['vertices'][triangle])
-
-            grad_test_elem=B_D @ grad_test
-      
-            v0= J*tf.reduce_sum(w_quad*grad_test_elem[0]*grad_elem)
-            v1= J*tf.reduce_sum(w_quad*grad_test_elem[1]*grad_elem)
-            v2= J*tf.reduce_sum(w_quad*grad_test_elem[2]*grad_elem)
-
-            l0=J*tf.reduce_sum(w_quad*grad_test_elem[3]*grad_elem)
-            l1=J*tf.reduce_sum(w_quad*grad_test_elem[4]*grad_elem)
-            l2=J*tf.reduce_sum(w_quad*grad_test_elem[5]*grad_elem)
-
-
-
-            #a_vertices[triangle[0]]+=a_element[0]
-            
-            """
-            if (self.mesh['vertex_markers'][triangle[0]]==0):
-                   # tf.tensor_scatter_nd_update(a_vertices, [triangle[0],1], v0)
-                    a_vertices[triangle[0],index].assign(v0)
-
-
-            if (self.mesh['vertex_markers'][triangle[1]]==0):
-                   # tf.tensor_scatter_nd_update(a_vertices, [triangle[1],1], v1)
-                    a_vertices[triangle[1],index].assign(v1)
- 
-
-            if (self.mesh['vertex_markers'][triangle[2]]==0):
-                  #  tf.tensor_scatter_nd_update(a_vertices, [triangle[2],1], v2)
-                    a_vertices[triangle[2],index].assign(v2)
-
-            if(self.b.r>=2):
-                    if(self.mesh['edge_markers'][self.mesh['edges_index_inside_triangle'][index][0]]==0):
-                       # tf.tensor_scatter_nd_update(a_edges, [self.mesh['edges_index_inside_triangle'][index][0],1], l0)
-                        a_edges[self.mesh['edges_index_inside_triangle'][index][0],index].assign(l0)
-
-                    if(self.mesh['edge_markers'][self.mesh['edges_index_inside_triangle'][index][1]]==0):
-                        #tf.tensor_scatter_nd_update(a_edges, [self.mesh['edges_index_inside_triangle'][index][1],1], l1)
-                        a_edges[self.mesh['edges_index_inside_triangle'][index][1],index].assign(l1)
-
-                    if(self.mesh['edge_markers'][self.mesh['edges_index_inside_triangle'][index][2]]==0):
-                        #tf.tensor_scatter_nd_update(a_edges, [self.mesh['edges_index_inside_triangle'][index][2],1], l2)
-                        a_edges[self.mesh['edges_index_inside_triangle'][index][2],index].assign(l2)
-
-            """
-
-            """
-            for r in range(self.b.n):
-                a_element[r]= J*tf.reduce_sum(w_quad*grad_test_elem[r]*grad_elem)
-
-
-
-            if (self.mesh['vertex_markers'][triangle[0]]==0):
-                    a_vertices[triangle[0]]+=a_element[0]
-
-
-            if (self.mesh['vertex_markers'][triangle[1]]==0):
-                    a_vertices[triangle[1]]+=a_element[1]
- 
-
-            if (self.mesh['vertex_markers'][triangle[2]]==0):
-                    a_vertices[triangle[2]]+=a_element[2]
-
-
-     
-            if(self.b.r>=2):
-                    if(self.mesh['edge_markers'][self.mesh['edges_index_inside_triangle'][index][0]]==0):
-                        a_edges[self.mesh['edges_index_inside_triangle'][index][0]]+=a_element[3]
-
-                    if(self.mesh['edge_markers'][self.mesh['edges_index_inside_triangle'][index][1]]==0):
-                            a_edges[self.mesh['edges_index_inside_triangle'][index][1]]+=a_element[4]
-
-                    if(self.mesh['edge_markers'][self.mesh['edges_index_inside_triangle'][index][2]]==0):
-                        a_edges[self.mesh['edges_index_inside_triangle'][index][2]]+=a_element[5]
-
-            #print(F_total_edges)
-            """
-            break
-
-
-        #tf.reduce_sum(a_vertices,axis=1,keepdims=True)-F_total_vertices,tf.reduce_sum(a_edges,axis=1,keepdims=True)-F_total_edges
-        return tf.reduce_sum(v0+v1+v2+l0+l1+l2)
-    
-
-
-
-
     @tf.function
     def loss_total(self):
         loss_b = self.boundary_loss()
@@ -384,17 +261,14 @@ class VPINN(tf.keras.Model):
         return loss
 
     def train(self, iter):
-
-
-        
         #self.a_vertices = tf.Variable(tf.zeros((self.n_vertices,self.n_triangles),dtype=tf.float64)) #
         #self.a_edges = tf.Variable(tf.zeros((self.n_edges,self.n_triangles),dtype=tf.float64)) #
 
         history = []
 
 
-        self.sum_of_vectors_vertices = tf.Variable(tf.zeros((self.n_vertices,1),dtype=tf.float64)) #
-        self.sum_of_vectors_edges = tf.Variable(tf.zeros((self.n_edges,1),dtype=tf.float64))       #
+        self.sum_of_vectors_vertices = tf.Variable(tf.zeros((self.n_vertices,1),dtype=tf_type)) #
+        self.sum_of_vectors_edges = tf.Variable(tf.zeros((self.n_edges,1),dtype=tf_type))       #
 
         start_time = time.time()
         for i in range(iter+1):
@@ -413,29 +287,18 @@ class VPINN(tf.keras.Model):
 
         return history
 
-    def get_domain_info(self):
 
-        a = np.array(self.params['domain'][0])
-        b = np.array(self.params['domain'][1])
-
-        scale = b - a
-        mid = (a + b)*0.5
-
-        return a, b, scale, mid
 
 
 
     def generate_boundary_points(self):
         # Boundary points
-        a = (0, 0)
-        b = (1, 0)
-        c = (1, 1)
-        d = (0, 1)
+        a, b, c, d =self.mesh['domain']
+   
 
+        boundary_points = self.generate_rectangle_points(a, b, c, d, self.params['n_bound']+1)
 
-        boundary_points = self.generate_rectangle_points(a, b, c, d, self.params['n_bound'])
-
-        self.boundary_points=tf.constant(boundary_points,dtype=tf.float64)
+        self.boundary_points=tf.constant(boundary_points,dtype=tf_type)
 
 
         u_bound_exact = self.pb.u_exact(self.boundary_points[:, 0], self.boundary_points[:, 1])
@@ -443,21 +306,6 @@ class VPINN(tf.keras.Model):
 
 
 
-
-
-    # def generate_inner_points(self):
-    #     _, _, scale, mid = self.get_domain_info()
-
-    #     #NOTE: probably using wrong constant here
-    #     temp = np.array(scale*(lhs(2, self.params['n_bound']) - 0.5) + mid)
-
-    #     self.xf = temp[:, 0]
-    #     self.yf = temp[:, 1]
-    #     self.X_f_train = np.hstack((self.xf[:, None], self.yf[:, None]))
-
-    #     ff = np.asarray([self.pb.f_exact(self.xf[j], self.yf[j])
-    #                     for j in range(len(self.yf))])
-    #     self.f_train = ff[:, None]
 
     def pre_compute(self):
 
@@ -471,19 +319,9 @@ class VPINN(tf.keras.Model):
 
 
     def evaluate_test_and_inter_functions(self):
-        #change that by using interpolator class
-        #self.v_evaluations = {}
-        #self.v_evaluations["vx_quad"] = self.pb.v(self.x_quad, self.y_quad, self.n_test)
-        #self.n_test = np.shape(self.v_evaluations["vx_quad"])[0]
-        # print(self.n_test)
-        # self.v_evaluations["dv_x_quad"], self.v_evaluations["d2v_x_quad"] = self.pb.dtest_func(self.n_test, self.x_quad)
-        # self.v_evaluations["vy_quad"] = self.pb.v_y(self.n_test, self.y_quad)
-        # self.v_evaluations["dv_y_quad"], self.v_evaluations["d2v_y_quad"] = self.pb.dtest_func(self.n_test, self.y_quad)
-        # print(np.max(self.v_evaluations["v_x_quad"]), np.min(self.v_evaluations["v_x_quad"]), np.sum(self.v_evaluations["v_x_quad"]))
-        # print(np.max(self.v_evaluations["v_y_quad"]), np.min(self.v_evaluations["v_y_quad"]), np.sum(self.v_evaluations["v_y_quad"]))
+      
 
-
-        self.b=interpolator(self.params['N_test'],False,True,points=self.points)
+        self.b=interpolator(self.params['n_test'],False,True,points=self.points)
 
         self.B=interpolator(2,False,False,points=None)
 
@@ -494,6 +332,8 @@ class VPINN(tf.keras.Model):
             grad.append(elem)
 
         self.grad_test=tf.constant(grad)
+
+
 
     def generate_points_on_edge(self,point1, point2, num_points):
         x_vals = np.linspace(point1[0], point2[0], num_points,endpoint=False)
@@ -531,22 +371,7 @@ class VPINN(tf.keras.Model):
         
         self.w_quad = np.reshape(self.w_quad, (len(self.w_quad), 1))
 
-    def pre_compute(self):
-        self.generate_quadrature_points()
-        self.evaluate_test_and_inter_functions()
-        self.construct_RHS()
-
-    def evaluate_test_functions(self):
-        self.v_evaluations = {}
-        self.v_evaluations["vx_quad"] = self.pb.v(self.x_quad, self.y_quad, self.n_test)
-        self.n_test = np.shape(self.v_evaluations["vx_quad"])[0]
-        # print(self.n_test)
-        # self.v_evaluations["dv_x_quad"], self.v_evaluations["d2v_x_quad"] = self.pb.dtest_func(self.n_test, self.x_quad)
-        # self.v_evaluations["vy_quad"] = self.pb.v_y(self.n_test, self.y_quad)
-        # self.v_evaluations["dv_y_quad"], self.v_evaluations["d2v_y_quad"] = self.pb.dtest_func(self.n_test, self.y_quad)
-        # print(np.max(self.v_evaluations["v_x_quad"]), np.min(self.v_evaluations["v_x_quad"]), np.sum(self.v_evaluations["v_x_quad"]))
-        # print(np.max(self.v_evaluations["v_y_quad"]), np.min(self.v_evaluations["v_y_quad"]), np.sum(self.v_evaluations["v_y_quad"]))
-
+ 
     def construct_RHS(self):
         #modify also this 
 
@@ -641,23 +466,29 @@ class VPINN(tf.keras.Model):
 
 
 
-    def generate_test_points(self):
-        # Test points for plotting really
 
-        lower_bound, upper_bound, _, _ = self.get_domain_info()
 
-        delta_test = self.params['delta_test']
-        x_test = np.arange(lower_bound[0], upper_bound[0] + delta_test, delta_test)
-        y_test = np.arange(lower_bound[1], upper_bound[1] + delta_test, delta_test)
-        data_temp = np.asarray([[[x_test[i], y_test[j], self.pb.u_exact(x_test[i], y_test[j])]
-                                 for i in range(len(x_test))] for j in range(len(y_test))])
-    
-        n_points = len(y_test)
 
-        x_test = data_temp.flatten()[0::3]
-        y_test = data_temp.flatten()[1::3]
-        exact = data_temp.flatten()[2::3]
-        return np.hstack((x_test[:, None], y_test[:, None])), exact[:, None], n_points
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def plot_loss_history(self, loss_history, PLOT):
         fontsize = 24
@@ -678,33 +509,7 @@ class VPINN(tf.keras.Model):
             plt.show()
         
 
-    def plot_domain(self, PLOT):
 
-        a, b, s, _ = self.get_domain_info()
-
-        fontsize = 24
-        x_train_plot, y_train_plot = zip(*self.boundary_points)
-        fig, ax = plt.subplots(1)
-        
-        plt.scatter(x_train_plot, y_train_plot, color='red')
-        for xc in self.grid_x:
-            plt.axvline(x=xc, ymin=0.045, ymax=0.954, linewidth=1.5)
-        for yc in self.grid_y:
-            plt.axhline(y=yc, xmin=0.045, xmax=0.954, linewidth=1.5)
-
-        plt.xlim([a[0] - 0.05*s[0], b[0] + 0.05*s[0]])
-        plt.ylim([a[1] - 0.05*s[1], b[1] + 0.05*s[1]])
-        plt.xlabel('$x$', fontsize = fontsize)
-        plt.ylabel('$y$', fontsize = fontsize)
-        ax.locator_params(nbins=5)
-        plt.tick_params( labelsize = 20)
-        #fig.tight_layout()
-        fig.set_size_inches(w=11,h=11)
-
-        if PLOT == 'save':
-            plt.savefig('VPINN_domain.pdf')
-        else:
-            plt.show()
         
 
     def plot_prediction(self, PLOT):
